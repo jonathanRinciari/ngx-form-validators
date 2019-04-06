bef13539642470434e869f65a9c55b7af5c5962d
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-04-06 12:35:20 -0400

    feat: (Email) - Test

---
+++ projects/ngx-form-validators/src/lib/validators/email/email.validator.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { email } from './email.validator';
+import { FormControl } from '@angular/forms';
+
+describe('Email Validator', () => {
+  const error = {email: true};
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should return true', () => {
+    const control = new FormControl('jrinciari@gmail.com');
+    expect(email(control)).toEqual(error);
+  });
+
+  it('should return false', () => {
+    const control = new FormControl('jrinciari');
+    expect(email(control)).toBeNull();
+  });
+});
