c152f5db714002aa71eb4ccb3fb72fa6f114dee8
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-04-06 12:35:28 -0400

    feat: (Phone) - Test

---
+++ projects/ngx-form-validators/src/lib/validators/phone/phone.validator.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { phone } from './phone.validator';
+import { FormControl } from '@angular/forms';
+
+describe('Phone Validator', () => {
+  const error = {phone: true};
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should return true', () => {
+    const control = new FormControl('2037330596');
+    expect(phone('US')(control)).toEqual(error);
+  });
+
+  it('should return false', () => {
+    const control = new FormControl('203730596');
+    expect(phone('US')(control)).toBeNull();
+  });
+});
