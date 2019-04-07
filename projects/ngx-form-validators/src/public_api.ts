f96c51d8fdaec006214f8e68b5e8a45de1805b3e
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-04-06 22:33:24 -0400

    fix: Added validators to public_api

--- projects/ngx-form-validators/src/public_api.ts
+++ projects/ngx-form-validators/src/public_api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-form-validators
  */
 
-export * from './lib/ngx-form-validators.service';
-export * from './lib/ngx-form-validators.component';
-export * from './lib/ngx-form-validators.module';
+export * from './lib/validators/email/email.validator';
+export * from './lib/validators/phone/phone.validator';
+export * from './lib/validators/password/password.validator';
